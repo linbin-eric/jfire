a7ab23a5a14e2fb64c4185ec4bc4d1c7675fcbdb
eric <eric@jfire.cn>
2017-03-29 16:43:29 +0800

    将jel类放入

---
+++ src/main/java/com/jfireframework/jfire/cache/el/JelException.java
@@ -0,0 +1,19 @@
+package com.jfireframework.jfire.cache.el;
+
+public class JelException extends Exception
+{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    public JelException(Throwable e)
+    {
+        super(e);
+    }
+    
+    public JelException(String msg)
+    {
+        super(msg);
+    }
+}
