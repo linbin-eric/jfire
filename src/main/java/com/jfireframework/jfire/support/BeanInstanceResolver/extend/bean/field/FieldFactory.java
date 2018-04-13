48eccb43e63c2b0f732bac72d14b1c5052278470
eric <eric@jfire.cn>
2018-04-13 11:19:32 +0800

    使PropertyRead注解时，优先选择jvm的启动参数，其次是配置文件

--- src/main/java/com/jfireframework/jfire/support/BeanInstanceResolver/extend/bean/field/FieldFactory.java
+++ src/main/java/com/jfireframework/jfire/support/BeanInstanceResolver/extend/bean/field/FieldFactory.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.annotation.Resource;
+import com.jfireframework.baseutil.StringUtil;
 import com.jfireframework.baseutil.anno.AnnotationUtil;
 import com.jfireframework.baseutil.exception.JustThrowException;
 import com.jfireframework.jfire.kernel.BeanDefinition;
@@ -44,159 +45,164 @@ import com.jfireframework.jfire.support.BeanInstanceResolver.extend.bean.field.p
 
 public class FieldFactory
 {
-    
-    /**
-     * 根据配置信息和field上的注解信息,返回该bean所有的依赖注入的field
-     * 
-     * @return
-     */
-    public static List<DIField> buildDependencyFields(AnnotationUtil annotationUtil, Class<?> type, Map<String, BeanDefinition> beanDefinitions)
-    {
-        List<Field> fields = getAllFields(type);
-        List<DIField> list = new LinkedList<DIField>();
-        try
-        {
-            for (Field field : fields)
-            {
-                if (annotationUtil.isPresent(Resource.class, field))
-                {
-                    list.add(buildDependencyField(annotationUtil, field, beanDefinitions));
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            throw new JustThrowException(e);
-        }
-        return list;
-    }
-    
-    static List<Field> getAllFields(Class<?> ckass)
-    {
-        List<Field> list = new ArrayList<Field>();
-        while (ckass != Object.class)
-        {
-            for (Field each : ckass.getDeclaredFields())
-            {
-                list.add(each);
-            }
-            ckass = ckass.getSuperclass();
-        }
-        return list;
-    }
-    
-    private static DIField buildDependencyField(AnnotationUtil annotationUtil, Field field, Map<String, BeanDefinition> beanDefinitions) throws NoSuchMethodException, SecurityException
-    {
-        Class<?> type = field.getType();
-        DiResolver resolver;
-        if (type == List.class)
-        {
-            resolver = new ListDiResolver();
-        }
-        else if (type == Map.class)
-        {
-            resolver = new MapDiResolver();
-        }
-        else if (type.isInterface() || Modifier.isAbstract(type.getModifiers()))
-        {
-            resolver = new InterfaceDiResolver();
-        }
-        else
-        {
-            resolver = new BaseDiResolver();
-        }
-        resolver.initialize(field, annotationUtil, beanDefinitions);
-        return new DiFieldImpl(resolver, field);
-    }
-    
-    /**
-     * 根据配置文件，返回该bean所有的条件输入注入的field
-     * 
-     * @return
-     */
-    public static List<ParamField> buildParamField(AnnotationUtil annotationUtil, Class<?> type, Map<String, String> properties, ClassLoader classLoader)
-    {
-        List<Field> fields = getAllFields(type);
-        List<ParamField> list = new LinkedList<ParamField>();
-        for (Field field : fields)
-        {
-            if (annotationUtil.isPresent(PropertyRead.class, field))
-            {
-                PropertyRead propertyRead = annotationUtil.getAnnotation(PropertyRead.class, field);
-                String propertyName = propertyRead.value().equals("") ? field.getName() : propertyRead.value();
-                if (properties.containsKey(propertyName))
-                {
-                    list.add(buildParamField(field, properties.get(propertyName), annotationUtil));
-                }
-                else
-                {
-                    continue;
-                }
-            }
-        }
-        return list;
-    }
-    
-    private static Map<Class<?>, Class<? extends ParamResolver>> paramResolverTypes = new HashMap<Class<?>, Class<? extends ParamResolver>>();
-    static
-    {
-        paramResolverTypes.put(String.class, StringResolver.class);
-        paramResolverTypes.put(int[].class, IntArrayResolver.class);
-        paramResolverTypes.put(Integer.class, IntegerResolver.class);
-        paramResolverTypes.put(int.class, IntResolver.class);
-        paramResolverTypes.put(Long.class, WLongResolver.class);
-        paramResolverTypes.put(long.class, LongResolver.class);
-        paramResolverTypes.put(Boolean.class, WBooleanResolver.class);
-        paramResolverTypes.put(boolean.class, BooleanResolver.class);
-        paramResolverTypes.put(float.class, FloatResolver.class);
-        paramResolverTypes.put(Float.class, WFloatResolver.class);
-        paramResolverTypes.put(String[].class, StringArrayResolver.class);
-        paramResolverTypes.put(Set.class, SetResolver.class);
-        paramResolverTypes.put(Class.class, ClassResolver.class);
-        paramResolverTypes.put(File.class, FileResolver.class);
-        paramResolverTypes.put(byte[].class, ByteArrayResolver.class);
-        
-    }
-    
-    private static ParamField buildParamField(Field field, String value, AnnotationUtil annotationUtil)
-    {
-        if (annotationUtil.isPresent(UserDefinedResolver.class, field))
-        {
-            UserDefinedResolver resolver = annotationUtil.getAnnotation(UserDefinedResolver.class, field);
-            try
-            {
-                ParamResolver instance = resolver.value().newInstance();
-                instance.initialize(value, field);
-                return new ParamFieldImpl(field, instance);
-            }
-            catch (Exception e)
-            {
-                throw new JustThrowException(e);
-            }
-        }
-        else
-        {
-            Class<?> fieldType = field.getType();
-            ParamResolver resolver = null;
-            if (Enum.class.isAssignableFrom(fieldType))
-            {
-                resolver = new EnumResolver();
-            }
-            else if (paramResolverTypes.containsKey(fieldType))
-            {
-                Class<? extends ParamResolver> resolverType = paramResolverTypes.get(fieldType);
-                try
-                {
-                    resolver = resolverType.newInstance();
-                }
-                catch (Exception e)
-                {
-                    throw new JustThrowException(e);
-                }
-            }
-            resolver.initialize(value, field);
-            return new ParamFieldImpl(field, resolver);
-        }
-    }
-    
+	
+	/**
+	 * 根据配置信息和field上的注解信息,返回该bean所有的依赖注入的field
+	 * 
+	 * @return
+	 */
+	public static List<DIField> buildDependencyFields(AnnotationUtil annotationUtil, Class<?> type, Map<String, BeanDefinition> beanDefinitions)
+	{
+		List<Field> fields = getAllFields(type);
+		List<DIField> list = new LinkedList<DIField>();
+		try
+		{
+			for (Field field : fields)
+			{
+				if (annotationUtil.isPresent(Resource.class, field))
+				{
+					list.add(buildDependencyField(annotationUtil, field, beanDefinitions));
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			throw new JustThrowException(e);
+		}
+		return list;
+	}
+	
+	static List<Field> getAllFields(Class<?> ckass)
+	{
+		List<Field> list = new ArrayList<Field>();
+		while (ckass != Object.class)
+		{
+			for (Field each : ckass.getDeclaredFields())
+			{
+				list.add(each);
+			}
+			ckass = ckass.getSuperclass();
+		}
+		return list;
+	}
+	
+	private static DIField buildDependencyField(AnnotationUtil annotationUtil, Field field, Map<String, BeanDefinition> beanDefinitions) throws NoSuchMethodException, SecurityException
+	{
+		Class<?> type = field.getType();
+		DiResolver resolver;
+		if (type == List.class)
+		{
+			resolver = new ListDiResolver();
+		}
+		else if (type == Map.class)
+		{
+			resolver = new MapDiResolver();
+		}
+		else if (type.isInterface() || Modifier.isAbstract(type.getModifiers()))
+		{
+			resolver = new InterfaceDiResolver();
+		}
+		else
+		{
+			resolver = new BaseDiResolver();
+		}
+		resolver.initialize(field, annotationUtil, beanDefinitions);
+		return new DiFieldImpl(resolver, field);
+	}
+	
+	/**
+	 * 根据配置文件，返回该bean所有的条件输入注入的field
+	 * 
+	 * @return
+	 */
+	public static List<ParamField> buildParamField(AnnotationUtil annotationUtil, Class<?> type, Map<String, String> properties, ClassLoader classLoader)
+	{
+		List<Field> fields = getAllFields(type);
+		List<ParamField> list = new LinkedList<ParamField>();
+		for (Field field : fields)
+		{
+			if (annotationUtil.isPresent(PropertyRead.class, field))
+			{
+				PropertyRead propertyRead = annotationUtil.getAnnotation(PropertyRead.class, field);
+				String propertyName = propertyRead.value().equals("") ? field.getName() : propertyRead.value();
+				String jvmPropertyValue = System.getProperty(propertyName);
+				if (StringUtil.isNotBlank(jvmPropertyValue))
+				{
+					list.add(buildParamField(field, jvmPropertyValue, annotationUtil));
+				}
+				else if (properties.containsKey(propertyName))
+				{
+					list.add(buildParamField(field, properties.get(propertyName), annotationUtil));
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+		return list;
+	}
+	
+	private static Map<Class<?>, Class<? extends ParamResolver>> paramResolverTypes = new HashMap<Class<?>, Class<? extends ParamResolver>>();
+	static
+	{
+		paramResolverTypes.put(String.class, StringResolver.class);
+		paramResolverTypes.put(int[].class, IntArrayResolver.class);
+		paramResolverTypes.put(Integer.class, IntegerResolver.class);
+		paramResolverTypes.put(int.class, IntResolver.class);
+		paramResolverTypes.put(Long.class, WLongResolver.class);
+		paramResolverTypes.put(long.class, LongResolver.class);
+		paramResolverTypes.put(Boolean.class, WBooleanResolver.class);
+		paramResolverTypes.put(boolean.class, BooleanResolver.class);
+		paramResolverTypes.put(float.class, FloatResolver.class);
+		paramResolverTypes.put(Float.class, WFloatResolver.class);
+		paramResolverTypes.put(String[].class, StringArrayResolver.class);
+		paramResolverTypes.put(Set.class, SetResolver.class);
+		paramResolverTypes.put(Class.class, ClassResolver.class);
+		paramResolverTypes.put(File.class, FileResolver.class);
+		paramResolverTypes.put(byte[].class, ByteArrayResolver.class);
+		
+	}
+	
+	private static ParamField buildParamField(Field field, String value, AnnotationUtil annotationUtil)
+	{
+		if (annotationUtil.isPresent(UserDefinedResolver.class, field))
+		{
+			UserDefinedResolver resolver = annotationUtil.getAnnotation(UserDefinedResolver.class, field);
+			try
+			{
+				ParamResolver instance = resolver.value().newInstance();
+				instance.initialize(value, field);
+				return new ParamFieldImpl(field, instance);
+			}
+			catch (Exception e)
+			{
+				throw new JustThrowException(e);
+			}
+		}
+		else
+		{
+			Class<?> fieldType = field.getType();
+			ParamResolver resolver = null;
+			if (Enum.class.isAssignableFrom(fieldType))
+			{
+				resolver = new EnumResolver();
+			}
+			else if (paramResolverTypes.containsKey(fieldType))
+			{
+				Class<? extends ParamResolver> resolverType = paramResolverTypes.get(fieldType);
+				try
+				{
+					resolver = resolverType.newInstance();
+				}
+				catch (Exception e)
+				{
+					throw new JustThrowException(e);
+				}
+			}
+			resolver.initialize(value, field);
+			return new ParamFieldImpl(field, resolver);
+		}
+	}
+	
 }
